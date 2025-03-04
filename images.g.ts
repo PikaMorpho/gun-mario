4f949c6e4d3a604148b35b9c16b21c15fca712e2
PikaMorpho <526998@bsd48.org>
2025-03-04 14:06:22 -0800

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,50 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "koopa":return img`
+...1............
+..111...........
+.7e117..........
+.7e1177.........
+.7e1177.........
+.711177.........
+7771777.........
+7e77777.........
+777.77..77777...
+77..77.7577757..
+77..77.77575777.
+77.771777757117.
+.7.771777575717.
+...771575777575.
+..7711757777757.
+...717575777575.
+...715777575777.
+....17777757777.
+....117775757111
+...77111577711..
+..7777711111177.
+.77777.......777
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
